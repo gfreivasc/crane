830379e4f79d22de55d0f5645bf4c756395bdb72
Gabriel Vasconcelos <gabriel.vasconcelos1@loblaw.ca>
2024-02-06 19:04:39 -0500

    Updates Gradle to 8.6 (AGP -> 8.2.2)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,3 @@
-@file:Suppress("UnstableApiUsage")
-enableFeaturePreview("VERSION_CATALOGS")
 dependencyResolutionManagement {
   repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
   repositories {
